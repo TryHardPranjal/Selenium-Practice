cd12fe64ee738ca25ec3bf1d5f79bd7a01ac9551
TryHardPranjal <pspsinha77@gmail.com>
2025-07-10 18:53:20 +0530

    New Practice on locators and a new assignment

---
+++ Practise/E2ETesting.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver=webdriver.Chrome()
+driver.implicitly_wait(3)
+driver.get("https://rahulshettyacademy.com/angularpractice/")
